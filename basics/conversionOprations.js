0fa953ca4ca7a51c5ad2fe23ce72e60eed2a809d
shubhangi149 <shubhangiraundal2160@gmail.com>
2025-07-29 22:18:00 +0530

    convertion to datatypes

---
+++ basics/conversionOprations.js
@@ -0,0 +1,45 @@
+/*let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//let valueInNumber = String(score)
+let valueInNumber = Boolean(score)
+console.log(typeof  valueInNumber);
+console.log(valueInNumber);*/
+
+
+// when we conver this type of string like "33" this is easily covert into number
+//"33nhiuhj"=> this type of no give the value "NaN" it means not anumber
+//but the type of NaN is number this concept create littel bit of confussion.
+//true => this value convert into "1"
+//false => this value convert into "0"
+// "shubhangi"=> this value convert into NaN
+//undefined => is also having value NaN
+// null => this value convert into "0"
+
+
+let isLoggedIn = 1
+
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1 => converts in true
+//0 => converts in false
+//"" empty string => converts in false
+//"shubhs"  full string=> converts in true
+
+
+
+let someNumber = 88
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
