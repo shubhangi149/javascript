d098d5a180ab20981943f0405126039518f56efd
shubhangi149 <shubhangiraundal2160@gmail.com>
2025-08-04 16:27:19 +0530

    maths function

---
+++ basics/nums_and_math.js
@@ -0,0 +1,40 @@
+// const score = 200 // that type of typing number is simple type
+// console.log(score);
+// const balance = new Number(400) // it is s specific type to show this is number 
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // kisibhi number ke bad kitne 0 chahiye ye isse fix kr skte
+// const num = 123.98765
+// console.log(num.toPrecision(2));
+
+// const num1 = 100000
+// console.log(num1.toLocaleString('en-IN')); // local no deta jaise ki 1 thousad me 3 0 aate to 1 ke bad comma deta
+
+//++++++++++++++++++++++++++++++++++++++++++++   maths    ++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));  //only use for convert negative to positive
+
+// console.log(Math.round(4.8)); // use for given no`s round value like 2.8=3, 8.9=9
+
+// console.log(Math.ceil(5.3));// take nearest higher value 
+// console.log(Math.floor(5.3));// take nearest lower value 
+// console.log(Math.min(3,6,8,9,5)); // use to find minimum value
+// console.log(Math.max(3,6,8,9,5));//use to find maximum value
+
+
+// console.log(Math.random()); //gives random value like 0.8765 1.876543
+// console.log((Math.random()*10)+1); 
+// console.log(Math.floor(Math.random()*10)+1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random()*(max-min +1)) +min);
+
+console.log(Math.round(Math.random()*10)+1);
+
+
+
